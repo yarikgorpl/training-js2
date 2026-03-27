952080e207eb87b12765cb9ed856eebaa96bfcef
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2026-03-28 00:30:51 +0200

    Fainish the first task
    Created images gallery and used library basicLightbox

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from './gallery-items.js';
+
+console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const murkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>     
+  </a>
+</li>`;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', murkup);
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  const { target } = event;
+
+  if (!target.classList.contains('gallery__image')) {
+    return;
+  }
+  const original = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${original}" width="800" height="600">`);
+  instance.show();
+  onEscClick(instance);
+}
+function onEscClick(param) {
+  document.addEventListener('keydown', event => {
+    if (event.code !== 'Escape') {
+      return;
+    }
+    param.close();
+  });
+}
